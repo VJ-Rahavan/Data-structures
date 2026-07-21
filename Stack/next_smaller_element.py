814d953615fc326c076c828721c5c8876e424e9b
Vijayarahavan K <vijayarahavan176@gmail.com>
2026-07-21 09:01:26 +0530

    feat: next smaller element

---
+++ Stack/next_smaller_element.py
@@ -0,0 +1,14 @@
+def next_smaller_element(arr):
+    stack = []
+    result = [-1] * len(arr)
+
+    for i in range(len(arr) - 1, -1, -1):
+        while stack and stack[-1] > arr[i]:
+            stack.pop()
+
+        if stack:
+            result[i] = stack[-1]
+
+        stack.append(arr[i])
+
+    return result
