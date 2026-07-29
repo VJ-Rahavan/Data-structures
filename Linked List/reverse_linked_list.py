8989934b646b89b8ad80164f56c8441b202c3d10
Vijayarahavan K <vijayarahavan176@gmail.com>
2026-07-29 07:37:37 +0530

    feat: reverse a linked list

--- Linked List/reverse_linked_list.py
+++ Linked List/reverse_linked_list.py
@@ -1,30 +1,80 @@
-# Reverse a linked list 
-
 class Node:
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
         self.next = None
 
-# New linked list is created with the reversed order of the original linked list. 
-# The function takes the head of the original linked list as input 
-# and returns the head of the new reversed linked list.
-def reverse_linked_list(data):
-    if not data:
-        return
-    
-    cur = data.next
-    new_ll = Node(data.data)
-    
-    while cur:
-        temp = Node(cur.data)
-        temp.next = new_ll
-        new_ll = temp
-        cur = cur.next
-    
-    return new_ll
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_end(self, data):
+        new_node = Node(data)
+
+        if not self.head:
+            self.head = new_node
+            return
+
+        cur = self.head
+        while cur.next:
+            cur = cur.next
+
+        cur.next = new_node
+
+    def reverse_copy(self):
+        cur = self.head
+        new_head = None
+
+        while cur:
+            temp = Node(cur.data)     # Create a new node
+            temp.next = new_head      # Insert at the front
+            new_head = temp           # Update new head
+            cur = cur.next            # Move to next node
+
+        return new_head
+
+    def reverse(self):
+        prev = None
+        cur = self.head
+
+        while cur:
+            next_node = cur.next      # Save next node
+            cur.next = prev           # Reverse the pointer
+            prev = cur                # Move prev forward
+            cur = next_node           # Move current forward
+
+        self.head = prev              # Update the head
+
+    def display(self, head=None):
+        if head is None:
+            head = self.head
+
+        cur = head
+        while cur:
+            print(cur.data, end=" -> ")
+            cur = cur.next
+        print("None")
 
 
-head = Node(4)
-head.next = Node(2)
+# ------------------------
+# Driver Code
+# ------------------------
 
-reverse_linked_list(head)
+ll = LinkedList()
+
+ll.insert_at_end(1)
+ll.insert_at_end(2)
+ll.insert_at_end(3)
+ll.insert_at_end(4)
+ll.insert_at_end(5)
+
+print("Original Linked List:")
+ll.display()
+
+new_head = ll.reverse_copy()
+
+print("New Reversed Linked List:")
+ll.display(new_head)
+
+print("Original Linked List (Still Unchanged):")
+ll.display()
