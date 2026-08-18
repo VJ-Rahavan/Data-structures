b83de00a1d383d6b9f46dc0601669dded5ad5c8e
Vijayarahavan K <vijayarahavan176@gmail.com>
2026-08-18 11:12:39 +0530

    feat: Iterative DFS

--- Graph/basic_dfs.py
+++ Graph/basic_dfs.py
@@ -25,3 +25,33 @@ def dfs(graph, start):
 
 
 dfs(graph, 0)
+
+#Iterative DFS Approach:
+
+graph = {
+    0: [1, 2],
+    1: [0, 3],
+    2: [0, 4],
+    3: [1],
+    4: [2]
+}
+
+def dfs(graph, start):
+    visited = set()
+    stack = [start]
+
+    while stack:
+        node = stack.pop()
+
+        if node in visited:
+            continue
+
+        visited.add(node)
+        print(node)
+
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                stack.append(neighbor)
+
+
+dfs(graph, 0)
