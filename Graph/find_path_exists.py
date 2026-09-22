c3ba931dde35ec08f4284b50e195c50b79246acf
Vijayarahavan K <vijayarahavan176@gmail.com>
2026-09-22 08:50:37 +0530

    feat: find if path exists

--- Graph/find_path_exists.py
+++ Graph/find_path_exists.py
@@ -1,10 +1,11 @@
+from collections import defaultdict
+
+
 class Solution:
     def validPath(
         self, n: int, edges: List[List[int]], source: int, destination: int
     ) -> bool:
-        graph = {}
-        for i in range(n):
-            graph[i] = []
+        graph = defaultdict(list)
 
         for edge in edges:
             node_a, node_b = edge
@@ -12,15 +13,16 @@ class Solution:
             graph[node_b].append(node_a)
 
         visited = set()
+        visited.add(source)
         
         def dfs(node):
 
-            visited.add(node)
             if destination == node:
                 return True
             
             for i in graph[node]:
                 if i not in visited:
+                    visited.add(i)
                     if dfs(i):
                         return True
                 
@@ -28,3 +30,30 @@ class Solution:
 
 
         return dfs(source)
+
+#BFS approach
+
+
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        graph = defaultdict(list)
+
+        for edge in edges:
+            node_a, node_b = edge
+            graph[node_a].append(node_b)
+            graph[node_b].append(node_a)
+
+        queue = deque([source])
+        visited = {source}
+
+        while queue:
+            current = queue.popleft()
+            if current == destination:
+                return True
+
+            for neighbor in graph[current]:
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    queue.append(neighbor)
+
+        return False    
