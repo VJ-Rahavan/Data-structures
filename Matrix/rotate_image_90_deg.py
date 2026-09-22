4efeccc92cb85fbeedc5d6d7aa33ee55bfae0f2b
Vijayarahavan K <vijayarahavan176@gmail.com>
2026-09-22 07:30:34 +0530

    feat: rotate image by 90 deg

---
+++ Matrix/rotate_image_90_deg.py
@@ -0,0 +1,16 @@
+# 48. Rotate Image
+
+class Solution:
+    def rotate(self, matrix: list[list[int]]) -> None:
+
+        n = len(matrix)
+
+        # 1. Transpose
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        # 2. Reverse every row
+        for row in matrix:
+            row.reverse()
+        
